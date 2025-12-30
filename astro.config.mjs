b4c9abd6985d333e4d917e86acf8b053091c22ef
Federico <federico@asso360.it>
2025-12-30 14:17:15 +0100

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,21 +2,41 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-import { languages } from './src/i18n/languages';
+import { defaultLang, languages } from './src/i18n/languages';
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fedegrad-asso360.github.io',
   base: 'via-sette-santi',
+
   i18n: {
     defaultLocale: 'en',
-    locales : Object.keys(languages),
+    locales: Object.keys(languages),
     routing: {
       prefixDefaultLocale: true, // Consigliato per coerenza: /it/, /en/, /es/
       fallbackType: 'rewrite'    // Se manca una traduzione, mostra quella di default senza redirect
     }
   },
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [sitemap({
+    i18n: {
+      defaultLocale: defaultLang,
+      // map a dictionary of language like { en: 'en-GB', it: 'it-IT', ecc.}
+      locales: Object.fromEntries(
+        Object.entries(languages).map(([langKey, langObj]) => [
+          langKey,
+          langObj.locale,
+        ])
+      )
+    },
+    changefreq: 'weekly',
+    priority: 0.7,
+    lastmod: new Date('2025-12-30T00:00:00Z')
+  })]
 });
--- src/i18n/languages.ts
+++ src/i18n/languages.ts
@@ -1,7 +1,7 @@
 export const languages = {
-  en: { label: 'English', flag: '🇬🇧' },
-  it: { label: 'Italiano', flag: '🇮🇹' },
-  de: { label: 'Deutsch', flag: '🇩🇪' },
+  en: { label: 'English', flag: '🇬🇧', locale: 'en-US' },
+  it: { label: 'Italiano', flag: '🇮🇹', locale: 'it-IT' },
+  de: { label: 'Deutsch', flag: '🇩🇪', locale: 'de-DE' },
 };
 
 export const defaultLang = 'en';
